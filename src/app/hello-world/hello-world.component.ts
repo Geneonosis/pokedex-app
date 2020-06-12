afb03f87ea93ce33a45ac2a0c60efd103f2f5804
Gene Tigner <genetigner.art@gmail.com>
2020-06-11 23:41:42 -0500

    pokedex updates

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,4 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+export interface pokemon {
+  position: number;
+  name: string;
+  pic: string;
+}
+
+// const apiClient = axios.create({
+
+// })
+
+let isLoaded: boolean = false;
+
+const POKEMON_DATA: pokemon[] = [
+  // {position: 1, name: 'Bulbasaur', pic: '001'},
+  // {position: 2, name: 'Ivysaur', pic: '002'},
+  // {position: 3, name: 'Venusaur', pic: '003'},
+  // {position: 4, name: 'Charmander', pic: '004'},
+  // {position: 5, name: 'Charmeleon', pic: '005'},
+  // {position: 6, name: 'Charizard', pic: '006'},
+  // {position: 7, name: 'Squirtle', pic: '007'},
+  // {position: 8, name: 'Wartortle', pic: '008'},
+  // {position: 9, name: 'Blastoise', pic: '009'},
+];
+
+interface RootObject {
+  results: Result[];
+}
+
+interface Result {
+  name: string;
+}
 
 @Component({
   selector: 'app-hello-world',
@@ -7,15 +40,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelloWorldComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['position','name','pic'];
+  myDataArray = POKEMON_DATA;
+  count: number = 0;
+  picNumber: string = '000';
+  formDate: any;
+  isLoaded: boolean = false;
 
-  example: string = "Hello world";
-
-  OnClick(): void {
-    this.example = "other world";
-  }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<RootObject>('https://pokeapi.co/api/v2/pokemon?limit=010').subscribe(data => {
+      data.results.forEach(element => {
+        this.count++;
+        POKEMON_DATA.push({position: this.count, name: element.name, pic: '001'});
+      });
+    });
+
+    console.log(POKEMON_DATA)
+    this.isLoaded = true;
   }
 
 }
